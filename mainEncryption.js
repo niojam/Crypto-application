a560e0b64b78d49d02ed80b3939871b07d03507e
Nikita <niojam@ttu.ee>
2019-02-09 10:00:22 +0200

    Second part done 2.0 with copyright (Little design)

--- mainEncryption.js
+++ mainEncryption.js
@@ -77,7 +77,7 @@ function Crypt(message, alphabet, key, textToChange, encode) {
  * the server-side, but the defaults work in most cases.
  */
 
-var b64pad  = ""; /* base-64 pad character. "=" for strict RFC compliance   */
+var b64pad = ""; /* base-64 pad character. "=" for strict RFC compliance   */
 
 /*
  * These are the functions you'll usually want to call
@@ -87,32 +87,33 @@ function b64_sha512(s) {
     var textToChange = document.getElementById("source_text").value;
     document.getElementById("result").value = rstr2b64(rstr_sha512(str2rstr_utf8(textToChange)));
 }
+
 /*
  * Calculate the SHA-512 of a raw string
  */
-function rstr_sha512(s)
-{
+function rstr_sha512(s) {
     return binb2rstr(binb_sha512(rstr2binb(s), s.length * 8));
 }
 
 /*
  * Convert a raw string to a base-64 string
  */
-function rstr2b64(input)
-{
-    try { b64pad } catch(e) { b64pad=''; }
+function rstr2b64(input) {
+    try {
+        b64pad
+    } catch (e) {
+        b64pad = '';
+    }
     var tab = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
     var output = "";
     var len = input.length;
-    for(var i = 0; i < len; i += 3)
-    {
+    for (var i = 0; i < len; i += 3) {
         var triplet = (input.charCodeAt(i) << 16)
-            | (i + 1 < len ? input.charCodeAt(i+1) << 8 : 0)
-            | (i + 2 < len ? input.charCodeAt(i+2)      : 0);
-        for(var j = 0; j < 4; j++)
-        {
-            if(i * 8 + j * 6 > input.length * 8) output += b64pad;
-            else output += tab.charAt((triplet >>> 6*(3-j)) & 0x3F);
+            | (i + 1 < len ? input.charCodeAt(i + 1) << 8 : 0)
+            | (i + 2 < len ? input.charCodeAt(i + 2) : 0);
+        for (var j = 0; j < 4; j++) {
+            if (i * 8 + j * 6 > input.length * 8) output += b64pad;
+            else output += tab.charAt((triplet >>> 6 * (3 - j)) & 0x3F);
         }
     }
     return output;
@@ -122,38 +123,35 @@ function rstr2b64(input)
  * Encode a string as utf-8.
  * For efficiency, this assumes the input is valid utf-16.
  */
-function str2rstr_utf8(input)
-{
+function str2rstr_utf8(input) {
     var output = "";
     var i = -1;
     var x, y;
 
-    while(++i < input.length)
-    {
+    while (++i < input.length) {
         /* Decode utf-16 surrogate pairs */
         x = input.charCodeAt(i);
         y = i + 1 < input.length ? input.charCodeAt(i + 1) : 0;
-        if(0xD800 <= x && x <= 0xDBFF && 0xDC00 <= y && y <= 0xDFFF)
-        {
+        if (0xD800 <= x && x <= 0xDBFF && 0xDC00 <= y && y <= 0xDFFF) {
             x = 0x10000 + ((x & 0x03FF) << 10) + (y & 0x03FF);
             i++;
         }
 
         /* Encode output as utf-8 */
-        if(x <= 0x7F)
+        if (x <= 0x7F)
             output += String.fromCharCode(x);
-        else if(x <= 0x7FF)
-            output += String.fromCharCode(0xC0 | ((x >>> 6 ) & 0x1F),
-                0x80 | ( x         & 0x3F));
-        else if(x <= 0xFFFF)
+        else if (x <= 0x7FF)
+            output += String.fromCharCode(0xC0 | ((x >>> 6) & 0x1F),
+                0x80 | (x & 0x3F));
+        else if (x <= 0xFFFF)
             output += String.fromCharCode(0xE0 | ((x >>> 12) & 0x0F),
-                0x80 | ((x >>> 6 ) & 0x3F),
-                0x80 | ( x         & 0x3F));
-        else if(x <= 0x1FFFFF)
+                0x80 | ((x >>> 6) & 0x3F),
+                0x80 | (x & 0x3F));
+        else if (x <= 0x1FFFFF)
             output += String.fromCharCode(0xF0 | ((x >>> 18) & 0x07),
                 0x80 | ((x >>> 12) & 0x3F),
-                0x80 | ((x >>> 6 ) & 0x3F),
-                0x80 | ( x         & 0x3F));
+                0x80 | ((x >>> 6) & 0x3F),
+                0x80 | (x & 0x3F));
     }
     return output;
 }
@@ -162,24 +160,22 @@ function str2rstr_utf8(input)
  * Convert a raw string to an array of big-endian words
  * Characters >255 have their high-byte silently ignored.
  */
-function rstr2binb(input)
-{
+function rstr2binb(input) {
     var output = Array(input.length >> 2);
-    for(var i = 0; i < output.length; i++)
+    for (var i = 0; i < output.length; i++)
         output[i] = 0;
-    for(var i = 0; i < input.length * 8; i += 8)
-        output[i>>5] |= (input.charCodeAt(i / 8) & 0xFF) << (24 - i % 32);
+    for (var i = 0; i < input.length * 8; i += 8)
+        output[i >> 5] |= (input.charCodeAt(i / 8) & 0xFF) << (24 - i % 32);
     return output;
 }
 
 /*
  * Convert an array of big-endian words to a string
  */
-function binb2rstr(input)
-{
+function binb2rstr(input) {
     var output = "";
-    for(var i = 0; i < input.length * 32; i += 8)
-        output += String.fromCharCode((input[i>>5] >>> (24 - i % 32)) & 0xFF);
+    for (var i = 0; i < input.length * 32; i += 8)
+        output += String.fromCharCode((input[i >> 5] >>> (24 - i % 32)) & 0xFF);
     return output;
 }
 
@@ -187,10 +183,9 @@ function binb2rstr(input)
  * Calculate the SHA-512 of an array of big-endian dwords, and a bit length
  */
 var sha512_k;
-function binb_sha512(x, len)
-{
-    if(sha512_k == undefined)
-    {
+
+function binb_sha512(x, len) {
+    if (sha512_k == undefined) {
         //SHA512 constants
         sha512_k = new Array(
             new int64(0x428a2f98, -685199838), new int64(0x71374491, 0x23ef65cd),
@@ -248,14 +243,14 @@ function binb_sha512(x, len)
 
     var T1 = new int64(0, 0),
         T2 = new int64(0, 0),
-        a = new int64(0,0),
-        b = new int64(0,0),
-        c = new int64(0,0),
-        d = new int64(0,0),
-        e = new int64(0,0),
-        f = new int64(0,0),
-        g = new int64(0,0),
-        h = new int64(0,0),
+        a = new int64(0, 0),
+        b = new int64(0, 0),
+        c = new int64(0, 0),
+        d = new int64(0, 0),
+        e = new int64(0, 0),
+        f = new int64(0, 0),
+        g = new int64(0, 0),
+        h = new int64(0, 0),
         //Temporary variables not specified by the document
         s0 = new int64(0, 0),
         s1 = new int64(0, 0),
@@ -266,14 +261,14 @@ function binb_sha512(x, len)
         r3 = new int64(0, 0);
     var j, i;
     var W = new Array(80);
-    for(i=0; i<80; i++)
+    for (i = 0; i < 80; i++)
         W[i] = new int64(0, 0);
 
     // append padding to the source string. The format is described in the FIPS.
     x[len >> 5] |= 0x80 << (24 - (len & 0x1f));
-    x[((len + 128 >> 10)<< 5) + 31] = len;
+    x[((len + 128 >> 10) << 5) + 31] = len;
 
-    for(i = 0; i<x.length; i+=32) //32 dwords is the block size
+    for (i = 0; i < x.length; i += 32) //32 dwords is the block size
     {
         int64copy(a, H[0]);
         int64copy(b, H[1]);
@@ -284,32 +279,29 @@ function binb_sha512(x, len)
         int64copy(g, H[6]);
         int64copy(h, H[7]);
 
-        for(j=0; j<16; j++)
-        {
-            W[j].h = x[i + 2*j];
-            W[j].l = x[i + 2*j + 1];
+        for (j = 0; j < 16; j++) {
+            W[j].h = x[i + 2 * j];
+            W[j].l = x[i + 2 * j + 1];
         }
 
-        for(j=16; j<80; j++)
-        {
+        for (j = 16; j < 80; j++) {
             //sigma1
-            int64rrot(r1, W[j-2], 19);
-            int64revrrot(r2, W[j-2], 29);
-            int64shr(r3, W[j-2], 6);
+            int64rrot(r1, W[j - 2], 19);
+            int64revrrot(r2, W[j - 2], 29);
+            int64shr(r3, W[j - 2], 6);
             s1.l = r1.l ^ r2.l ^ r3.l;
             s1.h = r1.h ^ r2.h ^ r3.h;
             //sigma0
-            int64rrot(r1, W[j-15], 1);
-            int64rrot(r2, W[j-15], 8);
-            int64shr(r3, W[j-15], 7);
+            int64rrot(r1, W[j - 15], 1);
+            int64rrot(r2, W[j - 15], 8);
+            int64shr(r3, W[j - 15], 7);
             s0.l = r1.l ^ r2.l ^ r3.l;
             s0.h = r1.h ^ r2.h ^ r3.h;
 
-            int64add4(W[j], s1, W[j-7], s0, W[j-16]);
+            int64add4(W[j], s1, W[j - 7], s0, W[j - 16]);
         }
 
-        for(j = 0; j < 80; j++)
-        {
+        for (j = 0; j < 80; j++) {
             //Ch
             Ch.l = (e.l & f.l) ^ (~e.l & g.l);
             Ch.h = (e.h & f.h) ^ (~e.h & g.h);
@@ -356,25 +348,22 @@ function binb_sha512(x, len)
 
     //represent the hash as an array of 32-bit dwords
     var hash = new Array(16);
-    for(i=0; i<8; i++)
-    {
-        hash[2*i] = H[i].h;
-        hash[2*i + 1] = H[i].l;
+    for (i = 0; i < 8; i++) {
+        hash[2 * i] = H[i].h;
+        hash[2 * i + 1] = H[i].l;
     }
     return hash;
 }
 
 //A constructor for 64-bit numbers
-function int64(h, l)
-{
+function int64(h, l) {
     this.h = h;
     this.l = l;
     //this.toString = int64toString;
 }
 
 //Copies src into dst, assuming both are 64-bit numbers
-function int64copy(dst, src)
-{
+function int64copy(dst, src) {
     dst.h = src.h;
     dst.l = src.l;
 }
@@ -382,32 +371,28 @@ function int64copy(dst, src)
 //Right-rotates a 64-bit number by shift
 //Won't handle cases of shift>=32
 //The function revrrot() is for that
-function int64rrot(dst, x, shift)
-{
-    dst.l = (x.l >>> shift) | (x.h << (32-shift));
-    dst.h = (x.h >>> shift) | (x.l << (32-shift));
+function int64rrot(dst, x, shift) {
+    dst.l = (x.l >>> shift) | (x.h << (32 - shift));
+    dst.h = (x.h >>> shift) | (x.l << (32 - shift));
 }
 
 //Reverses the dwords of the source and then rotates right by shift.
 //This is equivalent to rotation by 32+shift
-function int64revrrot(dst, x, shift)
-{
-    dst.l = (x.h >>> shift) | (x.l << (32-shift));
-    dst.h = (x.l >>> shift) | (x.h << (32-shift));
+function int64revrrot(dst, x, shift) {
+    dst.l = (x.h >>> shift) | (x.l << (32 - shift));
+    dst.h = (x.l >>> shift) | (x.h << (32 - shift));
 }
 
 //Bitwise-shifts right a 64-bit number by shift
 //Won't handle shift>=32, but it's never needed in SHA512
-function int64shr(dst, x, shift)
-{
-    dst.l = (x.l >>> shift) | (x.h << (32-shift));
+function int64shr(dst, x, shift) {
+    dst.l = (x.l >>> shift) | (x.h << (32 - shift));
     dst.h = (x.h >>> shift);
 }
 
 //Adds two 64-bit numbers
 //Like the original implementation, does not rely on 32-bit operations
-function int64add(dst, x, y)
-{
+function int64add(dst, x, y) {
     var w0 = (x.l & 0xffff) + (y.l & 0xffff);
     var w1 = (x.l >>> 16) + (y.l >>> 16) + (w0 >>> 16);
     var w2 = (x.h & 0xffff) + (y.h & 0xffff) + (w1 >>> 16);
@@ -417,8 +402,7 @@ function int64add(dst, x, y)
 }
 
 //Same, except with 4 addends. Works faster than adding them one by one.
-function int64add4(dst, a, b, c, d)
-{
+function int64add4(dst, a, b, c, d) {
     var w0 = (a.l & 0xffff) + (b.l & 0xffff) + (c.l & 0xffff) + (d.l & 0xffff);
     var w1 = (a.l >>> 16) + (b.l >>> 16) + (c.l >>> 16) + (d.l >>> 16) + (w0 >>> 16);
     var w2 = (a.h & 0xffff) + (b.h & 0xffff) + (c.h & 0xffff) + (d.h & 0xffff) + (w1 >>> 16);
